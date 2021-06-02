9c01a1508c3a3dfe55f1a4e080a33faa722fbc51
costfither <costabella_arnau@hotmail.com>
2021-06-02 07:13:42 +0200

    v01

---
+++ src/categoria.js
@@ -0,0 +1,5 @@
+const element1 = document.getElementById("categoriaUL");
+const element2 = document.getElementById("categoria");
+element2.innerText = element2.innerText + ": " + element1.childElementCount;
+
+
